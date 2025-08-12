6ad026ca8c9d9bd5ff026010bba42f5eb51feb28
Hyeonjunnn <dev_sklg0602@naver.com>
2025-08-12 18:23:12 +0900

    25.08.12 ProjectList 로딩창 적용

--- src/pages/ProjectList.jsx
+++ src/pages/ProjectList.jsx
@@ -1,45 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ProjectCard from '../components/ProjectCard';
 import { fetchProjects } from '../api/projectApi';
 import GlobalLoading from '../components/GlobalLoading';
+import { useQuery } from '@tanstack/react-query';
 
 const ProjectList = () => {
-  const [projects, setProjects] = useState([]);
-  const [error, setError] = useState(null);
   const size = 9;
 
-  useEffect(() => {
-    const loadProjects = async () => {
-      try {
-        setError(null);
-        const data = await fetchProjects(0, size);
-        setProjects(data.content || []);
-      } catch (err) {
-        setError('프로젝트를 불러오는 데 실패했습니다.');
-        console.error(err);
-      }
-    };
+  const {
+    data: projectsData,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ['projects', size],
+    queryFn: () => fetchProjects(0, size),
+  });
 
-    loadProjects();
-  }, []);
+  if (isLoading) return <GlobalLoading />;
 
-  if (error) return <div className="min-h-screen flex justify-center items-center text-red-600">{error}</div>;
+  if (isError) {
+    return (
+      <div className="min-h-screen flex justify-center items-center text-red-600">
+        프로젝트를 불러오는 데 실패했습니다.
+      </div>
+    );
+  }
+
+  const projects = projectsData?.content || [];
 
   return (
-    <>
-      <GlobalLoading />
-
-      <div className="min-h-screen bg-primary-50 py-12 px-6">
-        <h1 className="text-4xl font-bold text-center text-gray-900 mb-10">프로젝트</h1>
-        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
-          {projects
-            .sort((a, b) => b.projectNo - a.projectNo)
-            .map((project) => (
-              <ProjectCard key={project.projectNo} project={project} />
-            ))}
-        </div>
+    <div className="min-h-screen bg-primary-50 py-12 px-6">
+      <h1 className="text-4xl font-bold text-center text-gray-900 mb-10">프로젝트</h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {projects
+          .sort((a, b) => b.projectNo - a.projectNo)
+          .map((project) => (
+            <ProjectCard key={project.projectNo} project={project} />
+          ))}
       </div>
-    </>
+    </div>
   );
 };
 
